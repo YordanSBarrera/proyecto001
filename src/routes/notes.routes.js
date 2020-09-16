a26aec3c677e6534f0fa44631487ffae0f709b9d
YordanSBarrera <yordansaldanabarrera@gmail.com>
2020-09-16 10:53:11 -0300

    trabajando en el Edit

--- src/routes/notes.routes.js
+++ src/routes/notes.routes.js
@@ -12,6 +12,9 @@ router.get('/123', renderNoteForm);
 router.post('/notas/new-nota', createNewNote);
 router.get('/todo', renderNote);
 router.get('/notas/edit/:id', renderEditForm);
+//**//
+router.post('/notas/edit/:id', renderEditForm);
+
 router.delete('/notas/delete/:id', deleteNote);
 
 module.exports = router
